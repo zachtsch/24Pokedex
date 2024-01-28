a952d6ca7ad39ed159ca0e7a85b2d70c56826da4
zachtschetter <zachary.tschetter@usd.edu>
2024-01-28 17:09:30 -0600

    added expo project

---
+++ components/AssetExample.js
@@ -0,0 +1,32 @@
+import { Text, View, StyleSheet, Image } from 'react-native';
+
+
+export default function AssetExample() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>
+        Hello :)
+      </Text>
+      <Image style={styles.logo} source={require('../assets/snack-icon.png')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 24,
+  },
+  paragraph: {
+    margin: 24,
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  logo: {
+    height: 128,
+    width: 128,
+  }
+});
