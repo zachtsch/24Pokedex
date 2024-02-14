0bdf014d4a6914a13b43443b356e8148e36ce3df
zachtsch <zachary.tschetter@usd.edu>
2024-02-14 11:44:35 -0600

    navigate to pokemon

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button, Text } from 'react-native';
+import { Button, Text, Image } from 'react-native';
 const Stack = createNativeStackNavigator();
 
 const MyStack = () => {
@@ -14,17 +15,41 @@ const MyStack = () => {
           options={{ title: 'Welcome' }}
         />
         <Stack.Screen name='Profile' component={ProfileScreen} />
+        <Stack.Screen name='Pokemon' component={PokemonScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
+const PokemonScreen = ({ navigation, route }) => {
+  const [url, setURL] = useState('');
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.id}`)
+      .then((res) => res.json())
+      .then((r) => {
+        console.log(r.sprites.other['official-artwork'].front_shiny);
+        setURL(r.sprites.other['official-artwork'].front_shiny);
+      });
+  }, []);
+  return !url ? (
+    <></>
+  ) : (
+    <Image source={{ uri: url }} style={{ width: 300, height: 300 }} />
+  );
+};
+
 const HomeScreen = ({ navigation }) => {
   return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
-    />
+    <>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => navigation.navigate('Profile', { name: 'Jane' })}
+      />
+      <Button
+        title='Go to Pokemon profile'
+        onPress={() => navigation.navigate('Pokemon', { id: 3 })}
+      />
+    </>
   );
 };
 const ProfileScreen = ({ navigation, route }) => {
