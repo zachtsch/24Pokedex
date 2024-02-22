81f600a39d50ebe562ad52b3e46c35521f152bd7
Grant T Nelson <101273483+TwinCracker@users.noreply.github.com>
2024-02-22 10:40:04 -0600

    Revert "Feature/about component"

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, ScrollView } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
-
-import PokemonAboutTemplate from './components/AboutPage'; // Corrected import
-import PokemonDetail from './components/AboutPage';
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
 
 // or any files within the Snack
-
+import AssetExample from './components/AssetExample';
 
 export default function App() {
   return (
-    <PokemonDetail />
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>The App updates any time we save!</Text>
+      <Card>
+        <AssetExample />
+      </Card>
+    </View>
   );
-};
- 
+}
+
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
