eaa0a2591f0dd598ccf8d6319dd3bd1812c1164a
Aryan S <aryan@beaches.app>
2024-02-22 21:43:29 -0600

    Added functionality and styling to PokemonList

---
+++ components/PokemonCard.js
@@ -0,0 +1,133 @@
+import { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Card } from 'react-native-paper';
+
+import padId from '../lib/pad-id';
+import getBackgroundColor from '../lib/get-background-color';
+
+const PokemonCard = ({ name, url }) => {
+  const [pokemonData, setPokemonData] = useState();
+
+  useEffect(() => {
+    const getPokemonData = async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      setPokemonData(data);
+    };
+
+    getPokemonData();
+    return () => {
+      if (pokemonData) {
+        setPokemonData(null);
+      }
+    };
+  }, [name, url]);
+
+  return (
+    <View style={styles.cardContainer}>
+      {pokemonData && (
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.pokemonImage}
+            source={{
+              uri: pokemonData.sprites.other['official-artwork'].front_default,
+            }}
+          />
+          <View style={styles.idContainer}>
+            <Text style={{ fontSize: 12, color: 'grey', fontWeight: 'bold' }}>
+              {padId(pokemonData.id)}
+            </Text>
+          </View>
+        </View>
+      )}
+      <View style={styles.infoContainer}>
+        <View style={{ flex: 1, justifyContent: 'center' }}>
+          <Text style={styles.pokemonName}>{name}</Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'flex-end',
+            padding: 4,
+            gap: 5,
+            flexWrap: 'wrap',
+          }}
+        >
+          {pokemonData &&
+            pokemonData.types.map((value) => (
+              <View
+                key={value.type.name}
+                style={{
+                  backgroundColor: getBackgroundColor(value.type.name),
+                  borderRadius: 7,
+                  borderWidth: 2,
+                  borderColor: getBackgroundColor(value.type.name),
+                }}
+              >
+                <Text
+                  style={{
+                    textTransform: 'uppercase',
+                    padding: 2,
+                    color: 'white',
+                  }}
+                >
+                  {value.type.name}
+                </Text>
+              </View>
+            ))}
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    height: 250,
+    padding: 5,
+    width: '100%',
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    margin: 2,
+  },
+  imageContainer: {
+    flex: 2,
+    position: 'relative',
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+  infoContainer: {
+    flex: 1,
+    borderWidth: 1,
+    justifyContent: 'space-between',
+    width: '100%',
+    borderColor: 'black',
+  },
+  idContainer: {
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    paddingBottom: 2,
+    paddingRight: 4,
+  },
+  pokemonName: {
+    fontSize: 20,
+    paddingRight: 4,
+    textAlign: 'right',
+    textTransform: 'capitalize',
+    fontWeight: '400',
+  },
+  pokemonImage: {
+    width: 145,
+    height: 145,
+  },
+});
+
+export default PokemonCard;
---
+++ components/PokemonList.js
@@ -0,0 +1,44 @@
+import { FlashList } from '@shopify/flash-list';
+import { View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import PokemonCard from './PokemonCard';
+
+const PokemonList = () => {
+  const [pokedexData, setPokedexData] = useState();
+
+  useEffect(() => {
+    const getPokedexData = async () => {
+      const res = await fetch(
+        'https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0',
+      );
+      const data = await res.json();
+      setPokedexData(data.results);
+    };
+
+    getPokedexData();
+    return () => {
+      if (pokedexData) {
+        setPokedexData(null);
+      }
+    };
+  }, []);
+
+  return (
+    <View style={{ height: 500, width: '100%' }}>
+      {pokedexData ? (
+        <FlashList
+          data={pokedexData}
+          renderItem={(itemData) => (
+            <PokemonCard name={itemData.item.name} url={itemData.item.url} />
+          )}
+          estimatedItemSize={250}
+          numColumns={2}
+        />
+      ) : (
+        <Text>Loading...</Text>
+      )}
+    </View>
+  );
+};
+
+export default PokemonList;
