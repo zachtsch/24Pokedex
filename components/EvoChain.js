cbbb37432f5b6907e607d20746b42bf5f5bd621b
Brennan Malone <51138161+FalleyMalone@users.noreply.github.com>
2024-02-26 04:56:17 -0600

    Update EvoChain.js

--- components/EvoChain.js
+++ components/EvoChain.js
@@ -1,94 +1,72 @@
 import React from 'react';
-import { useEffect, useState, useRef } from 'react';
-import { StyleSheet, Text, View, Platform, SafeAreaView } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { getEvoData } from './EvoUtil';
 
-//Will be called with <EvoChain url={"pokemons url"}/>
-const EvoChain = ({ species, pokemon}) => {
+const EvoChain = ({ species }) => {
   const [evoInfo, setEvoInfo] = useState([]);
-  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getEvoInfo = async () => {
-      const evoData = await getEvoData(species.evolution_chain.url);
-      setEvoInfo(evoData);
+      setEvoInfo(await getEvoData(species.evolution_chain.url));
+    };
+    getEvoInfo();
+  }, [species]);
 
-      setLoading(false);
-    }
-
-    setLoading(true);
-    if (pokemon != null){
-      getEvoInfo();
-    }
-  }, [pokemon]);
-
-  return(
-    <View style={styles.evo-container}>
-      {!loading ? (evoInfo.map((row, i) => (
-        <React.Fragment key={i}>
-          <View style={`styles.evo-row ${
-            column.length === 2
-              ? "two-items"
-              : column.length > 2
-              ? "more-items"
-              : ""
-          }`}>
-
-          </View>
-        </React.Fragment>
-
-      )))}
+  return (
+    <View style={styles.pokeEvo}>
+      <Text style={styles.text}>Evolution </Text>
+      <View style={styles.evoRow}>
+        <Image source={{uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evoInfo[0]}.png`}} style={styles.evoSprite}/> 
+        <Image source={{uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evoInfo[1]}.png`}} style={styles.evoSprite}/>
+        <Image source={{uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${evoInfo[2]}.png`}} style={styles.evoSprite}/>
+      </View>
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   pokeEvo: {
     marginBottom: 0,
-    height: 145
-  },
-  pokeEvoH5: {
-    marginBottom: 5,
+    height: 145,
+    alignItems: 'center',
   },
   evoContainer: {
-    flex: 1,
-    justifyContent: 'center',
-  },
-  evoLoading: {
-    fontSize: 20,
-    paddingTop: 25,
-    width: 100,
+    
   },
   evoRow: {
-    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center',
-    margin: 5,
+    flexWrap: 'wrap',
+  },
+  evoRowTwoItems: {
+    
+  },
+  evoRowMoreItems: {
+    
   },
   evoArrow: {
-    marginRight: 0,
-    fontSize: 4,
-    color: 'black',
-    flex: 1,
-    alignItems: 'center',
+    
   },
   evoArrowSpan: {
-    marginBottom: 6,
+    
   },
   evoItem: {
-    backgroundColor: 'white',
-    borderRadius: 50,
-    padding: 15,
-    margin: 7,
-    cursor: 'pointer',
+    
   },
-  evoSprite,
   evoSpriteContainer: {
+    
+  },
+  evoSprite: {
     width: 100,
     height: 100,
+    margin: 5,
+    resizeMode: 'contain',
   },
-  
+  text: {
+    alignItems: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+  },
 });
 
-export default EvoChain; //Memo used to avoid reconstructing multiple times for each individual pokemon in the chain. Will end up using more mem tho
+export default EvoChain; 
