7956d37ff65cc7b1ecc6fbd046f7250d50665c00
Brennan Malone <51138161+FalleyMalone@users.noreply.github.com>
2024-03-03 22:36:33 -0600

    Create EvoUtil.js

---
+++ components/EvoUtil.js
@@ -0,0 +1,35 @@
+import getIdFromUrl from './get-id-from-url';
+
+const getEvoData = async (url) => {
+  const evos = [[],[],[]];
+  const fEvos = [];
+  const evoResponse = await fetch(url);
+  const evoData = await evoResponse.json();
+
+  const traverseEvoTree = (node, level) => {
+    if (node.length < 1) {
+      return null;
+    };
+
+    //Gets id from current node
+    const temp = getIdFromUrl(node.species.url)
+    evos[level].push(temp);
+
+    //Recursivly move on to next child
+    node.evolves_to.forEach((child) => {
+      traverseEvoTree(child, level + 1);
+    });
+  };
+
+  traverseEvoTree(evoData.chain, 0);
+
+  evos.forEach((child) => {
+    if (child.length != 0){
+      fEvos.push(child);
+    };
+  });
+
+  return fEvos;
+};
+
+export { getEvoData };
