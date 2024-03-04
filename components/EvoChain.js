553ce728ef769d437275a3a90ec34d90a8e62de1
Brennan Malone <51138161+FalleyMalone@users.noreply.github.com>
2024-03-04 11:24:08 -0600

    Update EvoChain.js

--- components/EvoChain.js
+++ components/EvoChain.js
@@ -1,33 +1,50 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import { getEvoData } from './EvoUtil';
 import usePokemon from '../hooks/dataFetching/usePokemon';
 import { useNavigation } from '@react-navigation/native';
+import EvoCard from './ui/EvoCard';
 
 const EvoChain = ({ species }) => {
   const [evoInfo, setEvoInfo] = useState([]);
+  const cardInfo = [[],[],[]];
   const navigation = useNavigation();
 
   useEffect(() => {
     const getEvoInfo = async () => {
       const temp = await getEvoData(species.evolution_chain.url)
-      console.log(temp);
       setEvoInfo(temp);
     };
+
     getEvoInfo();
+
+    const getCardInfo = async () => {
+      evoInfo.forEach(async (child) => {
+        const layerInfo = [];
+        child.forEach(async (id) => {
+          const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+          const data = await res.json();
+          layerInfo.push(data)
+        })
+        cardInfo.push(layerInfo)
+      });
+      console.log(cardInfo)
+    };
+
+    getCardInfo();
   }, [species]);
 
   return (
     <View style={styles.pokeEvo}>
-      <Text style={styles.text}>Evolution </Text>
+      <Text style={styles.text}>Evolution </Text>  
       <View style={styles.evoRow}>
         {evoInfo.map((child, depth) => (
           <React.Fragment key={depth}>
             {child.map((item, index) => (
-              <TouchableOpacity style={styles.evoSpriteContainer} onPress={() => {navigation.navigate('PokemonDetail', {pokemonData: usePokemon(item)})}}>
+              <EvoCard onPress={() => {navigation.navigate('PokemonDetail', {pokemonData: cardInfo[depth][index]})}}>
                 <Image key={item} style={styles.evoSprite} source={{uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${child[index]}.png`}}/>
-              </TouchableOpacity>
+              </EvoCard>
             ))}
             {depth !== evoInfo.length - 1 && ( <Text style={styles.text}>{`->`}</Text> )}
           </React.Fragment>
@@ -54,16 +71,6 @@ const styles = StyleSheet.create({
     height: 100,
     margin: 5,
     resizeMode: 'contain',
-  },
-  evoSpriteContainer: {
-    width: 120,
-    height: 120,
-    margin: 5,
-    backgroundColor: 'white',
-    padding: 5,
-    borderWidth: 1,
-    borderColor: '#c9c9c9',
-    borderRadius: 10,
   },
   text: {
     alignItems: 'center',
